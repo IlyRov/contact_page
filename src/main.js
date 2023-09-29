dc2a6a02be2c43bfd3b4a47375cca3f8c735381c
IlyRov <127045335+IlyRov@users.noreply.github.com>
2023-09-29 17:28:47 +0100

    update

--- src/main.js
+++ src/main.js
@@ -1,15 +1,14 @@
-
 function initMap() {
-    var myLatLng = { lat: 50.42496555405263, lng: 30.53408527758109 }; // Замените этими координатами на нужные вам
+    var myLatLng = { lat: 50.42496555405263, lng: 30.53408527758109 };
     var map = new google.maps.Map(document.getElementById('map'), {
       center: myLatLng,
-      zoom: 10 // Установите желаемый масштаб карты
+      zoom: 10 
     });
 
     var marker = new google.maps.Marker({
       position: myLatLng,
       map: map,
-      title: 'Моя метка' // Замените это на нужный вам заголовок метки
+      title: 'GoIT' 
     });
 }
 
