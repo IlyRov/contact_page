82e158fb622eaa96ab3c33c378e9420cbcc30354
IlyRov <127045335+IlyRov@users.noreply.github.com>
2023-09-30 23:39:02 +0100

    update

--- src/main.js
+++ src/main.js
@@ -1,15 +1,8 @@
 function initMap() {
-    var myLatLng = { lat: 50.42496555405263, lng: 30.53408527758109 };
-    var map = new google.maps.Map(document.getElementById('map'), {
-      center: myLatLng,
-      zoom: 10 
-    });
+    var uluru = { lat: 50.42496555405263, lng: 30.53408527758109 };
+    var map = new google.maps.Map(document.getElementById('map'), { zoom: 10, center: uluru });
 
-    var marker = new google.maps.Marker({
-      position: myLatLng,
-      map: map,
-      title: 'GoIT' 
-    });
+    var marker = new google.maps.Marker({ position: uluru, map: map});
 }
 
  
